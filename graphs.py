da46e043b948f92879c8a7215accf1a60b3b6345
dkazants <dmytro.kazantsev@gmail.com>
2019-11-10 15:11:48 +0100

    Moving conv_to_dict to class, adding method chk_if_broken

--- graphs.py
+++ graphs.py
@@ -4,31 +4,43 @@ edges = [(1,2),(1,3),(2,3),(3,4),(4,5),(4,6),(5,6),(5,7),(6,7),(7,8),(8,9),(8,10
 
 class Graphs():
 
-    def __init__(self, graph_dict=None):
-        if graph_dict == None:
-            graph_dict = {}
-        self.graph_dict = graph_dict
+    def __init__(self, edges_lst, nodes_num):
+        self.nodes = nodes_num
+        self.edges = edges_lst
+        self.__graph_dict = self.conv_to_dict(self.nodes, self.edges)
+
+    def conv_to_dict(self, nodes, edges):
+        graph_dict = {}
+        for node in range(nodes):
+            final_edges = list()
+            for edge in edges:
+                if node in edge:
+                    for n in edge:
+                        if n != node:
+                            final_edges.append(n)
+                            graph_dict[node] = final_edges
+        return graph_dict
 
     def __str__(self):
         a = ""
-        for k,v in self.graph_dict.items():
+        for k,v in self.__graph_dict.items():
             a += f"{k} :  {v}\n"
         return(str(a))
 
     def get_keys(self):
-        k_lst = []
-        for k,v in self.graph_dict.items():
-            k_lst.append(k)
-        return (k_lst)
+        self.k_lst = []
+        for k,v in self.nodes_rm_dic.items():
+            self.k_lst.append(k)
+        return (self.k_lst)
 
     def find_all_path(self,start_node,end_node, path=None):
         if path == None:
             path = []
-        gr = self.graph_dict
+        gr = self.nodes_rm_dic
         path = path + [start_node]
         if start_node == end_node:
             return [path]
-        if start_node not in self.graph_dict:
+        if start_node not in self.nodes_rm_dic:
             return []
         paths = []
         for node in gr[start_node]:
@@ -39,47 +51,29 @@ class Graphs():
         return(paths)
 
     def remove_node(self, node_num):
-        nodes_tmp1 = dict(self.graph_dict)
-        if node_num in nodes_tmp1:
-            nodes_tmp1.pop(node_num)
-        return(nodes_tmp1)
+        self.nodes_rm_dic = dict(self.__graph_dict)
+        if node_num in self.nodes_rm_dic:
+            self.nodes_rm_dic.pop(node_num)
 
+    def chk_if_broken(self,m):
+        self.remove_node(m)
+        for i in self.get_keys():
+            for j in self.get_keys():
+                if not self.find_all_path(i,j):
+                    return True
+                else:
+                    continue
+        return False
 
-def conv_to_dict(nodes, edges):
-    graph_dict = {}
-    for node in range(nodes):
-        final_edges = list()
-        for edge in edges:
-            if node in edge:
-                for n in edge:
-                    if n != node:
-                        final_edges.append(n)
-                        graph_dict[node] = final_edges
-    return graph_dict
-
-
-
-graph = conv_to_dict(nodes,edges)
-#for k,v in conv_to_dict(nodes, edges).items():
-#    print (k," : ", v)
-
-router = Graphs(graph)
+router = Graphs(edges, nodes)
 print(router)
 
-#print (router.find_all_path(0,6))
 affected_nodes = set()
 for remove_n in range(nodes):
-    r_node = router.remove_node(remove_n)
-    rm_router = Graphs(r_node)
-#    print(f"Removed node {remove_n} and new graph is " ,r_node)
-    list_of_keys = rm_router.get_keys()
-    for i in list_of_keys:
-        for j in list_of_keys:
-            if not rm_router.find_all_path(i,j):
-                 affected_nodes.add(remove_n)
-                 break
-            else:
-                continue
-            break
+    if router.chk_if_broken(remove_n):
+        affected_nodes.add(remove_n)
+        continue
+    else:
+        continue
 
 print(affected_nodes)
